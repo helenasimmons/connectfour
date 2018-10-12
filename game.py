a7c38f9d7e04afaac4345ca0a64c58ade52ebe1c
helenasimmons <helenacctx@gmail.com>
2018-10-12 14:11:39 -0500

    wip

--- game.py
+++ game.py
@@ -2,8 +2,16 @@ from board import Board
 from player import Player
 
 class Game:
-    def __init__(self)
+    def __init__(self):
+        #self.players = []
+        self.player1 = Player('red') #TEST STUFF. Del later. 
+        self.player2 = Player('yellow') #TEST STUFF. Del later. 
+        self.board = Board()
+        self.turn = 1 #Sets player1. Could be set to 2 if Player2's turn is up. 
+        
+    def play_game(self):
+        pass
     
 if __name__ == "__main__":
-    # test code
+    Game()
     pass
