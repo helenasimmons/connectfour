e1b73f4c9e696f4ee14b17b488b99952463ecce2
MIKHAYLOV.EGOR <2201136@fbisd.flourbluffschools.net>
2018-10-12 14:13:05 -0500

    added add_piece

--- board.py
+++ board.py
@@ -1,16 +1,35 @@
 #Board by Egor Mikhaylov
 
 class Board:
-    def __init__(self):
-        self.board_width = 7 #temp
-        self.board_height = 8 #temp
+    def __init__(self, width, height):
+        self.board_width = width
+        self.board_height = height
         self.board = [["o"]*self.board_width for i in range(self.board_height)]
     
-    def make_board(self):
+    def disp_board(self):
         for row in self.board:
             for element in row:
                 print(element, end=' ')
             print()
+        
+    def add_piece(self, col, piece):
+        placed = False
+        for row in range(len(self.board)):
+            for element in range(len(self.board[row])):
+                if element == col-1:
+                    if self.board[self.board_height-1][element] == "o" and placed == False:
+                        if piece == "plus":
+                            self.board[self.board_height-1][element] = "+"
+                        else:
+                            self.board[self.board_height-1][element] = "-"
+                        placed = True
+                    elif row != self.board_height-1 and placed == False:
+                        if self.board[row+1][element] != "o":
+                            if piece == "plus":
+                                self.board[row][element] = "+"
+                            else:
+                                self.board[row][element] = "-"
+                            placed = True
             
 
         
