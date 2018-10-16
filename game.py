7a0f94ab8c3931bfc355157bdfce926c52e061a2
helenasimmons <helenacctx@gmail.com>
2018-10-16 14:13:05 -0500

    wip

--- game.py
+++ game.py
@@ -4,13 +4,43 @@ from player import Player
 class Game:
     def __init__(self):
         self.players = []
-        #self.player1 = Player('red')
-        #self.player2 = Player('yellow') 
-        self.board = Board(width,hieght)
-        #self.turn = 1 #Sets player1. Could be set to 2 if Player2's turn is up. 
+        self.players.append(Player('x')) #player 1
+        self.players.append(Player('o')) #player 2
+        self.board = Board(7,7)
+        self.turn = 0
+        self.game = self.play_game()
         
     def play_game(self):
-        pass
+        print()
+        while True:
+            try:
+                self.board.disp_board()
+                self.choice = self.players[self.turn].get_choice()
+                self.board.add_piece(self.choice, self.players[self.turn].piece)
+                
+                
+                
+                self.board.check_win()
+                if self.board.check_win() == True:
+                    print('Game Over')
+                    break
+                if self.board.check_win() == False:
+                    self.turn += 1
+                if self.board.is_full() == True:
+                    pass
+                    
+                if self.board.is_full() == False:
+                    print('Game Over')
+                    
+                    
+                self.turn = self.turn % 2
+                break
+                
+            except Exception as e:
+                print()
+                print('That was not valid! {}'.format(str(e)))
+                print()
+                break
     
 if __name__ == "__main__":
     Game()
--- player.py
+++ player.py
@@ -2,16 +2,14 @@ class Player:
     def __init__(self,piece):
         self.name = self.get_name()
         self.piece = piece
-        self.choice = self.get_choice()
-        #self.current_choice = self.get_choice()
-        #self.turn = 1 #shouldn't the player set the turn. ALSO, for testing. Del if uncesessary. 
+        self.turn = 0 
         
     def get_name(self):
         self.name = input('What is your name? ')
+        return self.name
     def get_choice(self):
         self.choice = int(input('In which column do you want to place your piece? '))
-        
+        return int(self.choice)
 
 if __name__ == "__main__":
-    Player('red')
-    #pass
+    pass
