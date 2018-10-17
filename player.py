419f3cf6eb0fba30c218a1f5a77b947ebb7e40b5
helenasimmons <helenacctx@gmail.com>
2018-10-17 14:09:02 -0500

    almost finished

--- player.py
+++ player.py
@@ -2,14 +2,14 @@ class Player:
     def __init__(self,piece):
         self.name = self.get_name()
         self.piece = piece
-        self.turn = 0 
         
     def get_name(self):
         self.name = input('What is your name? ')
         return self.name
+    
     def get_choice(self):
-        self.choice = int(input('In which column do you want to place your piece? '))
-        return int(self.choice)
+        choice = int(input(f'{self.name} In which column do you want to place your piece? '))
+        return int(choice)
 
 if __name__ == "__main__":
     pass
